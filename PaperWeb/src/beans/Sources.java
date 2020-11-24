a122fd7288debfc41f637151aa5cfed718bdecef
yangyang-69 <1552007527@qq.com>
2020-11-24 23:23:46 +0800

    添加javabean的.0

---
+++ PaperWeb/src/beans/Sources.java
@@ -0,0 +1,39 @@
+package beans;
+
+public class Sources {
+	private int SourceID;
+	private String SourceName;
+	private String SourcePlace;
+	private String SourceURL;
+	private String SourceTel;
+	public int getSourceID() {
+		return SourceID;
+	}
+	public void setSourceID(int sourceID) {
+		SourceID = sourceID;
+	}
+	public String getSourceName() {
+		return SourceName;
+	}
+	public void setSourceName(String sourceName) {
+		SourceName = sourceName;
+	}
+	public String getSourcePlace() {
+		return SourcePlace;
+	}
+	public void setSourcePlace(String sourcePlace) {
+		SourcePlace = sourcePlace;
+	}
+	public String getSourceURL() {
+		return SourceURL;
+	}
+	public void setSourceURL(String sourceURL) {
+		SourceURL = sourceURL;
+	}
+	public String getSourceTel() {
+		return SourceTel;
+	}
+	public void setSourceTel(String sourceTel) {
+		SourceTel = sourceTel;
+	}
+}
---
+++ PaperWeb/src/beans/Users.java
@@ -0,0 +1,25 @@
+package beans;
+
+public class Users {
+	private int UserID;
+	private String UserName;
+	private String password;
+	public int getUserID() {
+		return UserID;
+	}
+	public void setUserID(int userID) {
+		UserID = userID;
+	}
+	public String getUserName() {
+		return UserName;
+	}
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
